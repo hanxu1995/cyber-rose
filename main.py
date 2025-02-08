c3007ccc95fef2006f662c9b4f2349ad936f6537
Xu Han <hanxu01995@gmail.com>
2025-02-08 16:50:19 -0500

    3d line flower

--- main.py
+++ main.py
@@ -18,27 +18,35 @@ def rotate(x, y, theta):
     return x_rotated, y_rotated
 
 
-def n_fold(x, y, n):
-    result = [rotate(x, y, theta) for theta in np.linspace(0, 2 * np.pi, n, endpoint=False)]
+def n_fold(x, y, n, turn=False):
+    phi = 0 if not turn else np.pi / n
+    result = [rotate(x, y, theta) for theta in np.linspace(0 + phi, 2 * np.pi + phi, n, endpoint=False)]
     return result
 
 
 if __name__ == '__main__':
     N = 1000
     num_pedals = 3
-    a1 = 0.8
+    a1s = [1.0, 0.9, 0.8, 0.7, 0.6]
     a2 = 0.5
-    a3 = 1
+    a3s = [2, 3, 4, 5, 6]
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
+    colors = plt.cm.viridis(np.linspace(0, 1, len(a3s)))
 
     x_orig = np.linspace(-1, 1, N)
-    x_pedal, y_pedal = pedal(x_orig, a1, a2)
-    result = n_fold(x_pedal, y_pedal, num_pedals)
 
-    for i in range(num_pedals):
-        z = a3 * (result[i][0] ** 2 + result[i][1] ** 2)
-        ax.plot3D(result[i][0], result[i][1], z, color='purple')
-    plt.axis('equal')
+    for j, a3 in enumerate(a3s):
+        x_pedal, y_pedal = pedal(x_orig, a1s[j], a2)
+        result = n_fold(x_pedal, y_pedal, num_pedals, turn=j % 2 == 1)
+        for i in range(num_pedals):
+            z = a3 * (result[i][0] ** 2 + result[i][1] ** 2)
+            ax.plot3D(result[i][0], result[i][1], z, color=colors[j])
+
+    ax.set_title("3D Layered Pedal Flower")
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.set_zlabel("Z-axis")
+    ax.view_init(elev=30, azim=45)
     plt.show()
