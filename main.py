e1a486c406c78fdb90b93e39fc626ddcade14fd3
Xu Han <hanxu01995@gmail.com>
2025-02-08 14:37:23 -0500

    3d paraboloid

--- main.py
+++ main.py
@@ -28,13 +28,27 @@ if __name__ == '__main__':
     num_pedals = 5
     a1 = 1.5
     a2 = 0.5
+    a3 = 1
 
-    x_orig = np.linspace(-1, 1, N)
-    x_pedal, y_pedal = pedal(x_orig, a1, a2)
-    result = n_fold(x_pedal, y_pedal, num_pedals)
+    # x_orig = np.linspace(-1, 1, N)
+    # x_pedal, y_pedal = pedal(x_orig, a1, a2)
+    # result = n_fold(x_pedal, y_pedal, num_pedals)
+    #
+    # for i in range(num_pedals):
+    #     plt.plot(result[i][0], result[i][1])
+    #     plt.fill(result[i][0], result[i][1], color='pink', alpha=0.5)
+    # plt.axis('equal')
+    # plt.show()
 
-    for i in range(num_pedals):
-        plt.plot(result[i][0], result[i][1])
-        plt.fill(result[i][0], result[i][1], color='pink', alpha=0.5)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x = np.linspace(-1, 1, N)
+    y = np.linspace(-1, 1, N)
+    X, Y = np.meshgrid(x, y)
+    Z = a3 * (X ** 2 + Y ** 2)
+    Z[X ** 2 + Y ** 2 > 1] = np.nan
+
+    ax.plot_surface(X, Y, Z, cmap='viridis')
     plt.axis('equal')
     plt.show()
