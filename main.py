bad74a44a4fa41f65209bee6df2c2901e7405d31
Xu Han <hanxu01995@gmail.com>
2025-02-08 17:56:42 -0500

    able to tune params

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def pedal(x, a1, a2):
+def pedal(a1, a2, c, N):
+    x_max = np.sqrt(c / (a1 + a2))
+    x = np.linspace(-x_max, x_max, N)
     y1 = a1 * x ** 2
-    y2 = -a2 * x ** 2 + a1 + a2
+    y2 = -a2 * x ** 2 + c
     x_concat = np.concatenate([x, x[::-1]])
     y_concat = np.concatenate([y1, y2[::-1]])
     return x_concat, y_concat
@@ -27,21 +29,20 @@ def n_fold(x, y, n, turn=False):
 if __name__ == '__main__':
     N = 1000
     num_pedals = 3
-    a1s = [1.0, 0.9, 0.8, 0.7, 0.6]
-    a2 = 0.5
-    a3s = [2, 3, 4, 5, 6]
+    a1s = [3, 4, 5]
+    a2s = [2, 2.67, 3.33]
+    a3s = [2.5, 3, 3.5]
+    cs = [2, 1.914, 1.857]
 
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
     colors = plt.cm.viridis(np.linspace(0, 1, len(a3s)))
 
-    x_orig = np.linspace(-1, 1, N)
-
-    for j, a3 in enumerate(a3s):
-        x_pedal, y_pedal = pedal(x_orig, a1s[j], a2)
+    for j in range(len(a1s)):
+        x_pedal, y_pedal = pedal(a1s[j], a2s[j], cs[j], N)
         result = n_fold(x_pedal, y_pedal, num_pedals, turn=j % 2 == 1)
         for i in range(num_pedals):
-            z = a3 * (result[i][0] ** 2 + result[i][1] ** 2)
+            z = a3s[j] * (result[i][0] ** 2 + result[i][1] ** 2)
             ax.plot3D(result[i][0], result[i][1], z, color=colors[j])
 
     ax.set_title("3D Layered Pedal Flower")
