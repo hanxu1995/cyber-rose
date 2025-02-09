575bf12d4817403e81585c8816d8928c8a06acba
Xu Han <hanxu01995@gmail.com>
2025-02-08 21:52:59 -0500

    add comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
+# single 2d pedal
 def pedal(a1, a2, c, N):
     x_max = np.sqrt(c / (a1 + a2))
     x = np.linspace(-x_max, x_max, N)
@@ -15,18 +16,22 @@ def pedal(a1, a2, c, N):
     return x_concat, y_concat
 
 
+# rotate a 2d shape by theta
 def rotate(x, y, theta):
     x_rotated = x * np.cos(theta) - y * np.sin(theta)
     y_rotated = x * np.sin(theta) + y * np.cos(theta)
     return x_rotated, y_rotated
 
 
+# make n copies of a 2d shape and rotate them by (k/n)*2pi
+# if turn=True, add phase phi=pi/n
 def n_fold(x, y, n, turn=False):
     phi = 0 if not turn else np.pi / n
     result = [rotate(x, y, theta) for theta in np.linspace(0 + phi, 2 * np.pi + phi, n, endpoint=False)]
     return result
 
 
+# a cylinder
 def stem(length, radius, num_points):
     z = np.linspace(0, -length, num_points)
     theta = np.linspace(0, 2 * np.pi, num_points)
@@ -36,6 +41,7 @@ def stem(length, radius, num_points):
     return x, y, z
 
 
+# an oval attached to the stem
 def leaf(starting_z, a, b, phi, num_points):
     t = np.linspace(0, 2 * np.pi, num_points)
     x = a + a * np.cos(t)
@@ -45,18 +51,19 @@ def leaf(starting_z, a, b, phi, num_points):
 
 
 if __name__ == '__main__':
-    N = 1000
-    num_pedals = 5
-    num_layers = 5
-    stem_height = 20
-    stem_radius = 0.4
-    a1s = np.linspace(1, 10, num_layers) / 4
-    a2s = a1s / 3
-    a3s = np.linspace(1, 10, num_layers) / 10
-    cs = np.sqrt(10 / a3s) * np.linspace(1, 1.25, num_layers)
+    N = 1000  # number of points in each np.linspace
+    num_pedals = 5  # number of pedals in a single layer
+    num_layers = 5  # number of layers
+    stem_length = 20  # length of stem
+    stem_radius = 0.4  # radius of stem
+    a1s = np.linspace(1, 10, num_layers) / 4  # param for pedal lower part
+    a2s = a1s / 3  # param for pedal upper part
+    a3s = np.linspace(1, 10, num_layers) / 10  # param for layer
+    cs = np.sqrt(10 / a3s) * np.linspace(1, 1.25, num_layers)  # param for pedal size
 
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
+    # rose-red colors
     colors = [
         (0.9, 0.4, 0.5),
         (0.8, 0.3, 0.4),
@@ -64,17 +71,25 @@ if __name__ == '__main__':
         (0.6, 0.1, 0.2),
         (0.5, 0.0, 0.1)
     ]
+    # opacity
     alphas = np.linspace(0.6, 0.9, num_layers)
 
     for j in range(num_layers):
+        # for each layer
+        # draw a 2d pedal
         x_pedal, y_pedal = pedal(a1s[j], a2s[j], cs[j], N)
+        # draw n 2d pedals and rotate them
         result = n_fold(x_pedal, y_pedal, num_pedals, turn=j % 2 == 1)
         for i in range(num_pedals):
+            # for each pedal
+            # map the 2d pedal onto a 3d paraboloid
             z = a3s[j] * (result[i][0] ** 2 + result[i][1] ** 2)
+            # add color and opacity for the 3d pedal
             verts = [list(zip(result[i][0], result[i][1], z))]
             poly = Poly3DCollection(verts, facecolors=colors[j], alpha=alphas[j], edgecolor='gold', linewidths=1.5)
             ax.add_collection3d(poly)
-    x_stem, y_stem, z_stem = stem(stem_height, stem_radius, N)
+    # stem
+    x_stem, y_stem, z_stem = stem(stem_length, stem_radius, N)
     ax.plot_surface(x_stem, y_stem, z_stem, color='green', alpha=0.8)
 
     # leaf
@@ -83,6 +98,7 @@ if __name__ == '__main__':
     poly_leaf = Poly3DCollection(verts_leaf, facecolors='darkgreen', alpha=0.8, edgecolor='gold', linewidths=1.5)
     ax.add_collection3d(poly_leaf)
 
+    # labels and axis
     ax.set_title("3D Flower")
     ax.set_xlabel("X-axis")
     ax.set_ylabel("Y-axis")
